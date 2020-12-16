8488e44a6bcdd4673daefa01e213a385816201ed
khaled <khaled.s.abdellah@gmail.com>
2020-12-16 13:44:21 +0200

    add reservation doctor

--- routes/reservation.js
+++ routes/reservation.js
@@ -81,7 +81,7 @@ router.get('/', async (req, res, next)=> {
           let resultData = [];
           for(let obj in data){
             const userObj = await userModel.findOne({ _id: data[obj].userId }, { '_id':0,'password':0,'__v': 0 });
-            const doctorDataObj = await userModel.findOne({ _id: data[obj].doctorId }, { '_id':0,'password':0,'__v': 0 });
+            const doctorDataObj = await doctorModel.findOne({ _id: data[obj].doctorId }, { '_id':0,'password':0,'__v': 0 });
 
             if(userObj){
               resultData.push(
